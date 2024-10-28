e34eaf604839218c517f8099ba0491ab6ba4ede5
Marcel Stadelmann <m@marceli.to>
2024-10-28 16:43:51 +0100

    wip

---
+++ config/client.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'base_url' => env('APP_URL'),
+  'meta_description' => 'Die Werbeagentur setzt auf wirkungsvolle, überraschende Kommunikation, auf allen Kanälen der klassischen Werbung und neuen Medien. Wir legen Wert auf den persönlichen Kontakt und einer lösungsorientierten Zusammenarbeit. Mit klaren Strategien, Ideenreichtum und perfekten Umsetzungen setzen wir entscheidende Akzente, eindrücklich, einfach, einzigartig.'
+];
