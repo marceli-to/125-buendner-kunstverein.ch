cc5264e392c681cfa4c9b8efb080d6cc5b3f0299
Marcel Stadelmann <m@marceli.to>
2024-11-15 15:29:56 +0100

    wip

--- config/client.php
+++ config/client.php
@@ -1,5 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
-  'meta_description' => 'Die Werbeagentur setzt auf wirkungsvolle, überraschende Kommunikation, auf allen Kanälen der klassischen Werbung und neuen Medien. Wir legen Wert auf den persönlichen Kontakt und einer lösungsorientierten Zusammenarbeit. Mit klaren Strategien, Ideenreichtum und perfekten Umsetzungen setzen wir entscheidende Akzente, eindrücklich, einfach, einzigartig.'
+  'meta_description' => 'Im Jahr 2025 feiert der Bündner Kunstverein ein besonderes Jubiläum: 125 Jahre Leidenschaft für die bildende Kunst in Graubünden! Seit 1900 fördern wir die Kunst in Graubünden, bereichern das kulturelle Leben und setzen uns für den künstlerischen Austausch ein. Freuen Sie sich auf ein Jahr voller inspirierender Ausstellungen und Veranstaltungen.'
 ];
